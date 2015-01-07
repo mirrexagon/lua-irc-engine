897415388af00ab5dccae19e51fd5e987cf64603
Andrew Abbott <legospacy@gmail.com>
2015-01-07 16:48:02 +1100

    Make license pure MIT, add module meta fields
    Preparing for v1.0.0

--- irc-engine.lua
+++ irc-engine.lua
@@ -1,3 +1,30 @@
+local IRCe = {
+	_VERSION = "Lua IRC Engine",
+	_DESCRIPTION = "A Lua IRC module that tries to be minimal and extensible.",
+	_URL = "https://github.com/legospacy/lua-irc-engine",
+	_LICENSE = [[
+		Copyright (c) 2014-2015 Andrew Abbott
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in
+		all copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+		THE SOFTWARE.
+	]]
+}
+
 -- Localisations --
 local unpack = table.unpack or unpack
 -- ============= --
@@ -270,6 +297,6 @@ local function new()
 	}, Base)
 end
 
-return {
-	new = new
-}
+IRCe.new = new
+
+return IRCe
