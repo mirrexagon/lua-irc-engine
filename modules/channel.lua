6c33fed43b8eb50a7b5dbaf0355644a3f276461e
Andrew Abbott <legospacy@gmail.com>
2015-02-20 11:48:16 +1100

    Add TOPIC and MODE senders; expand MODE handler
    There are now separate callbacks for user and channel modes, and they
    receive the modes one at a time.
    
    I'm not sure how best to implement the MODE sender, this'll do for now.

--- modules/channel.lua
+++ modules/channel.lua
@@ -57,6 +57,14 @@ return {
 		end,
 
 
+		MODE = function(self, target, operation, modes, mode_params)
+			if mode_params then
+				return ("MODE %s %s %s"):format(target, operation .. modes, mode_params)
+			else
+				return ("MODE %s %s"):format(target, operation .. modes)
+			end
+		end,
+
 		TOPIC = function(self, channel, topic)
 			if topic then
 				return ("TOPIC %s :%s"):format(channel, topic)
@@ -93,9 +101,22 @@ return {
 				for i = 3, #params do
 					mode_params[i-2] = params[i]
 				end
+
+				-- Do the callback for each separate mode.
+				for i = 1, #modes do
+					self:handle("CHANNELMODE", sender, operation, modes[i], mode_params[i])
+				end
+
+				-- Legacy interface, may be removed.
 				return sender, operation, modes, mode_params
 			else
 				-- User mode.
+				-- Do the callback for each separate mode.
+				for i = 1, #modes do
+					self:handle("USERMODE", sender, operation, modes[i], target)
+				end
+
+				-- Legacy interface, may be removed.
 				return nil, operation, modes, target
 			end
 		end,
@@ -103,19 +124,28 @@ return {
 
 		-- RPL_NOTOPIC
 		["331"] = function(self, sender, params)
-			return sender, params[2], params[3]
+			local channel = params[2]
+			local message = params[3]
+
+			self:handle("TOPIC", channel, nil, message)
+
+			return sender, params
 		end,
 
 		-- RPL_TOPIC
 		["332"] = function(self, sender, params)
-			return sender, params[2], params[3]
+			local channel = params[2]
+			local topic = params[3]
+
+			self:handle("TOPIC", channel, topic)
+
+			return sender, params
 		end,
 
 
 		-- Channel names list.
 		-- RPL_NAMREPLY
 		["353"] = function(self, sender, params)
-			local target = params[1]
 			local kind = params[2]
 			local channel = params[3]
 			local list = string_explode(params[4])
@@ -139,7 +169,6 @@ return {
 
 		-- RPL_ENDOFNAMES
 		["366"] = function(self, sender, params)
-			local target = params[1]
 			local channel = params[2]
 			local message = params[3]
 
