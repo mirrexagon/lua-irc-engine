1171ddc1bde06fdcf1c01349d3b2110229952935
Andrew Abbott <legospacy@gmail.com>
2015-09-23 10:47:30 +1000

    Bump version to v2.1.0

--- irce.lua
+++ irce.lua
@@ -1,5 +1,5 @@
 local IRCe = {
-	_VERSION = "Lua IRC Engine v2.0.0",
+	_VERSION = "Lua IRC Engine v2.1.0",
 	_DESCRIPTION = "A Lua IRC module that tries to be minimal and extensible.",
 	_URL = "https://github.com/legospacy/lua-irc-engine",
 	_LICENSE = [[
